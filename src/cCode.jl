135f806d309331507229c6e381e912fd181e2bd6
Ander <andersalaberria@gmail.com>
2018-08-31 19:07:18 +0200

    C code fixed v6

--- src/cCode.jl
+++ src/cCode.jl
@@ -13,6 +13,6 @@ cKeplerFlow!(x_bek::Array{Float64,1}, v_bek::Array{Float64,1}, x0_bek::Array{Flo
 
     # definitu Julia funtzio bat, C funtzioa deitzen duena
     cKeplerFlow!(x_bek::Array{Float64,1}, v_bek::Array{Float64,1}, x0_bek::Array{Float64,1}, v0_bek::Array{Float64,1}, dt::Any, mu::Float64, traza::Int32) = 
-        ccall(("kepler", Clib), Void,(Ptr{Float64},Ptr{Float64},Ptr{Float64},Ptr{Float64},Float64,Float64,Int32), 
+        ccall(("kepler", Clib), Nothing,(Ptr{Float64},Ptr{Float64},Ptr{Float64},Ptr{Float64},Float64,Float64,Int32), 
         x_bek, v_bek, x0_bek, v0_bek, convert(Float64,dt), mu, traza)
 end
