ab61110059f7a09d8a06b12bab80a442c531b240
Ander <andersalaberria@gmail.com>
2018-08-31 18:08:20 +0200

    C code fixed v3

--- src/cCode.jl
+++ src/cCode.jl
@@ -1,17 +1,17 @@
-code = open(string(Base.source_path()[1:(end-8)],"kepler.c")) do f
-    code = readstring(f)
+open("kepler.c") do f
+    code = read(f, String)
+
+    # liburutegiaren PATH-a hemen gordeko da
+    Clib = tempname() 
+
+    # konpilatzeko, C-ko math liburutegia behar da
+    # liburutegia konpilatuko dugu, -lm
+    open(`gcc -fPIC -O3 -xc -shared -lm -o $(Clib * "." * Libdl.dlext) -`, "w") do f
+         print(f, code)
+    end
+
+    # definitu Julia funtzio bat, C funtzioa deitzen duena
+    global cKeplerFlow!(x_bek::Array{Float64,1}, v_bek::Array{Float64,1}, x0_bek::Array{Float64,1}, v0_bek::Array{Float64,1}, dt::Any, mu::Float64, traza::Int32) = 
+        ccall(("kepler", Clib), Void,(Ptr{Float64},Ptr{Float64},Ptr{Float64},Ptr{Float64},Float64,Float64,Int32), 
+        x_bek, v_bek, x0_bek, v0_bek, convert(Float64,dt), mu, traza)
 end
-
-# liburutegiaren PATH-a hemen gordeko da
-Clib = tempname() 
-
-# konpilatzeko, C-ko math liburutegia behar da
-# liburutegia konpilatuko dugu, -lm
-open(`gcc -fPIC -O3 -xc -shared -lm -o $(Clib * "." * Libdl.dlext) -`, "w") do f
-     print(f, code)
-end
-
-# definitu Julia funtzio bat, C funtzioa deitzen duena
-cKeplerFlow!(x_bek::Array{Float64,1}, v_bek::Array{Float64,1}, x0_bek::Array{Float64,1}, v0_bek::Array{Float64,1}, i::Any, mu::Float64, traza::Int32) = 
-    ccall(("kepler", Clib), Void,(Ptr{Float64},Ptr{Float64},Ptr{Float64},Ptr{Float64},Float64,Float64,Int32), 
-    x_bek, v_bek, x0_bek, v0_bek, convert(Float64,i), mu, traza)
