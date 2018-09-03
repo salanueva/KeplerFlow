1a0818f0b5ef55841222e282060d4049b4729f7c
Ander <andersalaberria@gmail.com>
2018-09-03 19:57:27 +0200

    Adding Documentation.

--- src/energy.jl
+++ src/energy.jl
@@ -18,7 +18,7 @@ Calculates the sum of kynetic and potential energy of the N-body hamiltonian sys
 ```math
 m'_{1} = m_{sum_{N}} \\\\
 m'_{i} = m_{i} \\frac{m_{sum_{i-1}}}{m_{sum_{i}}} \\\\
-\mathrm{where, } m_{sum_{i}} = \\sum_{j = 1}^{i} m_{j}
+\\mathrm{where, } m_{sum_{i}} = \\sum_{j = 1}^{i} m_{j}
 ```
 * `mu`: array of N elements, where element with index i contains the standard gravitational parameter of the i-th body
 ```math
@@ -26,9 +26,9 @@ mu_{i} = G \\cdot m_{j}
 ```
 * ``mu_{sum}``: array of N elements, where the standard gravitational parameter of the bodies from 1 to i is saved in the i-th element
 ```math
-mu_{sum_{i}} = \\sum_{j = 1}^{i} G \\cdot m[j]
+mu_{sum_{i}} = \\sum_{j = 1}^{i} G \\cdot m_{j}
 ```
-* `ignore_H0`: boolean that determines if Hamiltonian ``\mathcal{H_{0}}`` will be ignored (when calculating the energy).
+* `ignore_H0`: boolean that determines if Hamiltonian ``\\mathcal{H_{0}}`` will be ignored (when calculating the energy).
 
 # Returns 
 
