a98a93d04e6e7b8b785b675059864fb26a19eaa8
Ander <andersalaberria@gmail.com>
2018-09-03 19:43:41 +0200

    Adding Documentation.

--- src/coordinateTransform.jl
+++ src/coordinateTransform.jl
@@ -12,8 +12,9 @@ Transforms cartesian coordinates into jacobi coordinates.
 * `m`: array of N elements, where element number i contains the mass of i-th body.
 * `m_sum`: array of N elements, where element with index i contains the mass of the bodies from 1 to i
 ```math
-\\centering
+\\begin{center}
 m_{sum_{i}} = \\sum_{j = 1}^{i} m_{j} 
+\\end{center}
 ```
 """
 function cartesian2jacobi!(r, r_j, m, m_sum) 
